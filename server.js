ce61bc9eb6dad9b36b8991dcfb06686402182886
RM S <mixtop256@gmail.com>
2025-07-13 14:37:52 +0700

    update

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
 import cors from 'cors';
+import mysql from 'mysql';
 import loginRoutes from './login.js';
 import regisRoutes from './register.js';
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+});
+
+const database = 'CREATE DATABASE IF NOT EXISTS ??';
+const namedata = 'customer';
+try {
+    db.query(database, [namedata], (err) => {
+    if (err) throw err;
+    console.log('Database Created');
+    });
+} catch (err) {
+    if (err) throw err;
+    console.log('Not work');
+};
+
 
 const app = express();
 app.use(cors());
